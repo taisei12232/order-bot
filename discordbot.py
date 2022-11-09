0cec06d74f3ac83e57d461adab15704c54f24f41
taisei12232 <taisei12232@gmail.com>
2022-11-09 16:27:18 +0900

    update

--- discordbot.py
+++ discordbot.py
@@ -51,6 +51,7 @@ async def randdice(interaction: discord.Interaction,roll:str = "1D6"):
                 result += rand.randrange(int(digits[1])) + 1
         else:
             await interaction.response.send_message("ダイスの数は100以下、出目は1000以下に設定してください。")
+            return
         await interaction.response.send_message(result)
 
 
